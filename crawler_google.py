b61b207df10e982be484a67fdf13e4fd860aef90
Kyunghoon Jo <10jobss@gmail.com>
2017-10-23 15:11:13 +0900

    Add files via upload
    - variable tirm (age_limit, downloads, support_version
    - add time interval

--- crawler_google.py
+++ crawler_google.py
@@ -14,8 +14,9 @@ def get_info(start_url):
     driver = webdriver.Chrome('C:/Users/kyunghoon/Downloads/chromedriver_win32/chromedriver')
     driver.implicitly_wait(15)
     driver.get(start_url)
+    '''
     elm = driver.find_element_by_tag_name('html')
-   # i = 1
+
     for i in range(1, 6): # scroll down (1 ~ 300)
         elm.send_keys(Keys.END)
         time.sleep(10)
@@ -25,12 +26,12 @@ def get_info(start_url):
         elm.send_keys(Keys.END)
         time.sleep(10)
         driver.implicitly_wait(30)
-    
+    '''
     driver.implicitly_wait(10)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    driver.implicitly_wait(15) #
+    driver.implicitly_wait(15)
     main_content = soup.find('div', class_='body-content')
     apps = main_content.find_all('div', class_='card-content id-track-click id-track-impression')
     rank = 1    
@@ -50,31 +51,33 @@ def get_info(start_url):
         genre = main_content.find('div', class_='left-info').find('span', itemprop="genre").get_text()
         rating_score = main_content.find('div', class_='score').get_text()
         rating_count = main_content.find('div', class_='right-info').find('span', class_='rating-count').get_text()
-        print(name + " ** " + dev + " ** " + genre + " ** " + rating_score + " ** " + rating_count)
+        print(name + "***" + dev + "***" + genre + "***" + rating_score + "***" + rating_count)
         content_dis = main_content.find('div', class_='meta-info contains-text-link')
         containers = content_dis.find_all('div', {'class' : 'content'})
         age_limit = containers[0].get_text()
+        age_limit = age_limit[2:4]
         try:
             attribute = containers[1].get_text() 
         except:
             attribute = "NA"
             pass
-        print(age_limit + " *** " + attribute)
+        print(age_limit + "***" + attribute)
         meta_infos = main_content.find_all('div', {'class' : 'meta-info'})
         for meta_info in meta_infos:
             tmp = meta_info
             if tmp.find('div', class_='title').get_text() == "설치 수":
-                downloads = tmp.find('div', itemprop="numDownloads").get_text()
+                downloads = tmp.find('div', itemprop="numDownloads").get_text().strip()
             elif tmp.find('div', class_='title').get_text() == "지원되는 Android 버전":
                 support_version = tmp.find('div', itemprop="operatingSystems").get_text()
-        print(downloads + " *** " + support_version)
+                support_version = support_version.strip().split(' ')[0]
+        print(downloads + "***" + support_version)
         print(str(rank) + " Success!")
         
         tmp_dict = {'rank' : rank, 'name' : name, 'dev' : dev, 'genre' : genre, 'rating_score' : rating_score,
                     'rating_count' : rating_count, 'age_limit' : age_limit, 'attribute' : attribute,
                     'downloads' : downloads, 'support_version' : support_version}
         data_sets.append(tmp_dict)
-        time.sleep(5) #
+        time.sleep(5)
         rank += 1
         driver.execute_script("window.history.go(-1)")
     
@@ -84,7 +87,9 @@ data_sets = []
 get_info(start_url)
 
 data_keys = data_sets[0].keys()
-with open('test_item_matrix_2.csv', 'w') as csvfile:
+'''
+with open('google_game.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, delimiter=',',lineterminator='\n', fieldnames = data_keys)
     writer.writeheader()
     writer.writerows(data_sets)
+'''
